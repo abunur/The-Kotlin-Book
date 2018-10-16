b0f5fcaed7bd3cabca746ea02154c1ad667df8dc
Fuad Kamal <fuad@anaara.com>
2018-10-16 13:32:02 -0400

    update text
    wire up the UI so pressing Submit updates the textview with the text entered into the EditText

--- app/src/main/java/com/anaara/kotlinbookchapter3/MainActivity.kt
+++ app/src/main/java/com/anaara/kotlinbookchapter3/MainActivity.kt
@@ -2,11 +2,16 @@ package com.anaara.kotlinbookchapter3
 
 import android.app.Activity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        submitButton.setOnClickListener {
+          greetingLabel.text = userName.text
+        }
     }
 }
