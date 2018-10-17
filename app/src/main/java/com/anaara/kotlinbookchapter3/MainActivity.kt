5055a27ce03815e61a41952759bd039cc9428963
Fuad Kamal <fuad@anaara.com>
2018-10-17 14:13:12 -0400

    try catch
    error handling with try catch

--- app/src/main/java/com/anaara/kotlinbookchapter3/MainActivity.kt
+++ app/src/main/java/com/anaara/kotlinbookchapter3/MainActivity.kt
@@ -6,19 +6,27 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : Activity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        submitButton.setOnClickListener {
-            answerLabel.text = addTwoNumbers(firstNumber.text.toString(), secondNumber.text.toString())
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+
+    submitButton.setOnClickListener {
+      try {
+        val firstValue = firstNumber.text.toString().toInt()
+        val secondValue = secondNumber.text.toString().toInt()
+        val answerValue = addTwoNumbers(firstValue, secondValue)
+        answerLabel.text = "$answerValue"
+      } catch (e: NumberFormatException) {
+        answerLabel.text = e.toString()
+      }
 
 
-        }
     }
+  }
 
-    fun addTwoNumbers(firstValue: String, secondValue: String): String {
-        val answerValue = firstValue + secondValue
-        return  answerValue
+  fun addTwoNumbers(firstValue: Int, secondValue: Int): Int {
+    val answerValue = firstValue + secondValue
+    return answerValue
 
-    }
+  }
 }
