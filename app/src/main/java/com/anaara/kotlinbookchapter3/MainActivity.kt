93bba44d2f0c41d4d589f5b7602063f993808834
Fuad Kamal <fuad@anaara.com>
2018-12-27 12:18:51 -0500

    initial drawing
    draws a circle into an image view. Doesn’t appear until you hit the submit button for some reason, though.  Probably to do with the same issues with animations and when the UI gets updated.  Consider it a bug.  Also need to fix the sizing.

--- app/src/main/java/com/anaara/kotlinbookchapter3/MainActivity.kt
+++ app/src/main/java/com/anaara/kotlinbookchapter3/MainActivity.kt
@@ -31,7 +31,14 @@
 package com.anaara.kotlinbookchapter3
 
 import android.app.Activity
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
+import android.util.DisplayMetrics
+import android.widget.ImageView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : Activity() {
@@ -47,7 +54,35 @@ class MainActivity : Activity() {
       val answerValue = addTwoNumbers(firstValue, secondValue)
       answerLabel.text = " = $answerValue"
 
+      //drawing code
+      val bitmap = Bitmap.createBitmap(700, 1000, Bitmap.Config.ARGB_4444)
+      val canvas = Canvas(bitmap)
 
+      // canvas background color
+      canvas.drawARGB(255, 78, 168, 186);
+
+      var paint = Paint()
+      paint.setColor(Color.parseColor("#FFFFFF"))
+      paint.setStrokeWidth(30F)
+      paint.setStyle(Paint.Style.STROKE)
+      paint.setAntiAlias(true)
+      paint.setDither(true)
+
+      // get device dimensions
+      val displayMetrics = DisplayMetrics()
+      windowManager.defaultDisplay.getMetrics(displayMetrics)
+      // circle center
+      System.out.println("Width : "+displayMetrics.widthPixels)
+      var center_x = (displayMetrics.widthPixels/2).toFloat()
+      var center_y = (displayMetrics.heightPixels/2).toFloat()
+      var radius = 300F
+
+      // draw circle
+      canvas.drawCircle(center_x, center_y, radius, paint)
+      // now bitmap holds the updated pixels
+
+      // set bitmap as background to ImageView
+      drawArea.background = BitmapDrawable(getResources(), bitmap)
     }
   }
 
